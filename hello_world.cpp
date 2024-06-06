f03c0b9e00756b1bb590e289cdb06eb390812795
Morgikc <144792250+Morgikc@users.noreply.github.com>
2024-06-06 19:41:04 +0300

    add signs

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,6 @@ int main(int argc, char* argv[]) {
 std::cout << "Enter name:\n";
 std::string name;
 std::cin >> name; // enter name
-std::cout << "Hello world from " << name << std::endl;
+std::cout << "Hello, world, from " << name  << "." << std::endl;
 return 0;
 }

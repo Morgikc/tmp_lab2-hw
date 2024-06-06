2c0869943c21efee076b8cce4ce41718a8ee0878
Morgick <fedorenko2005anton@gmail.com>
2024-06-06 19:23:58 +0300

    add name input

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main(int argc, char* argv[]) {
-cout << "Hello world" << endl;
+int main(int argc. char* argv[]) {
+cout << "Enter name:\n";
+string name;
+cin >> name;
+cout << "Hello world from " << name << endl;
 return 0;
 }
